4965e8cc75582198822bca271471100814fe6d11
vishnuacappella <acappella.studiov@gmail.com>
2022-07-15 19:12:27 +0530

    Arithmetic Operatorscompleted

--- index.js
+++ index.js
@@ -140,3 +140,12 @@
 
 // *Arithmetic Operators
 
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 ** num2);
+console.log(num1 % num2);
