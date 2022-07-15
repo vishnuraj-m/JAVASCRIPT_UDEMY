6f3753ea52c3181a086a0af3b5769f2e6a84bcde
vishnuacappella <acappella.studiov@gmail.com>
2022-07-15 19:23:51 +0530

    Assignment Operator completed

--- index.js
+++ index.js
@@ -140,12 +140,38 @@
 
 // *Arithmetic Operators
 
-let num1 = 10;
-let num2 = 5;
+// let num1 = 10;
+// let num2 = 5;
 
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
-console.log(num1 ** num2);
-console.log(num1 % num2);
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 ** num2);
+// console.log(num1 % num2);
+
+
+// *Assignment Operators
+
+// let num1 = 10;
+// let num2 = num1;
+// console.log(num1, num2);
+
+
+let num1 = 10;
+let num2 = num1 + 5;
+num1 += 5;
+console.log(num2);
+
+num1 = num1 - 5;
+num1 -= 5;
+
+num1 = num1 * 5;
+num1 *= 5
+
+num1 = num1 + 5;
+num1 += 5;
+
+num1++;
+
+num1--;
