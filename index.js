d5acb320f35a986fbe6f9362f42ce8ba610d8210
vishnuacappella <acappella.studiov@gmail.com>
2022-07-16 16:59:48 +0530

    Exercise Grading system

--- index.js
+++ index.js
@@ -311,3 +311,25 @@ console.log(number % 3 === 0 && number % 5 === 0
             ? 'Buzz'
             : 'Nothing'
 );
+
+
+// !Exercise Grading system
+
+let mark = 90;
+if (mark < 0 || mark > 100) {
+    console.log('Invalid Mark');
+} else if (mark >= 80 && mark <= 100) {
+    console.log('A+');
+} else if (mark >= 70 && mark <= 79) {
+    console.log('A');
+} else if (mark >= 60 && mark <= 69) {
+    console.log('A-');
+} else if (mark >= 50 && mark <= 59) {
+    console.log('B');
+} else if (mark >= 40 && mark <= 49) {
+    console.log('C');
+} else if (mark >= 33 && mark <= 39) {
+    console.log('D');
+} else {
+    console.log('Fail')
+}
